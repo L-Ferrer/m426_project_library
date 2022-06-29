34acfff4dee14a1dd71bdcf31d5a311a03b099ae
L-Ferrer <leandro.ferrer2005@gmail.com>
2022-06-29 08:50:07 +0200

    Updated JavaDoc.

--- api/src/main/java/ch/bbw/m326/ProjectLibraryApplication.java
+++ api/src/main/java/ch/bbw/m326/ProjectLibraryApplication.java
@@ -1,15 +1,10 @@
 package ch.bbw.m326;
 
-import ch.bbw.m326.database.Library;
 import ch.bbw.m326.database.Manager;
-import ch.bbw.m326.database.Writer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Timer;
-import java.util.TimerTask;
 
 /**
  * @author Leandro Ferrer
