285c1508f10e9746dd103bc508fa724a0ad15ed0
L-Ferrer <leandro.ferrer2005@gmail.com>
2022-06-29 06:48:18 +0200

    Added library content table.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -13,21 +13,49 @@ function Dashboard() {
     }
     useEffect(() => {
         setUser(localStorage.getItem('user'));
-        fetch('http://localhost:8080/user/login/auth', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          }
+        fetch('http://localhost:8080/library', {
+          method: 'GET'
         })
-          .then(data => setData(data));
+            .then(res => res.json())
+            .then(data => setData(data))
+            .catch((err) => console.error(err));
     }, []);
 
     return (
         <>
             <div id="user">Currently logged in as:&nbsp;&nbsp;<b>{user}</b></div>
             <div className="content">
-                <h1>Library</h1>
+                <h1 id="library-title">Library content</h1>
+                <body class="large-screen">
+                <div class="wrap">
+                    <div class="table-wrapper">
+                    <table class="table-responsive card-list-table">
+                        <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Genre</th>
+                            <th>Type</th>
+                            <th>Year</th>
+                            <th>Description</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {data.map((value, i) => (
+                        <tr className="tableContent">
+                            <td>{value.title}</td>
+                            <td>{value.genre}</td>
+                            <td>{value.type}</td>
+                            <td>{value.year}</td>
+                            <td>{value.info}</td>
+                        </tr>
+                         ))}
+                        </tbody>
+                    </table>
+                    </div>
+                </div>
+                </body>
             </div>
+            
         </>
     )
 };
